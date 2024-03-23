ec90499ddd06c94f1b4c3c01f4773bba61ae60f7
Namhai01 <dgn3072000@gmail.com>
2024-03-24 02:09:21 +0700

    api

--- src/shared/constants/app.jsx
+++ src/shared/constants/app.jsx
@@ -2,6 +2,6 @@
 // export const BASE_URL = "http://localhost:3001/";
 // export const BASE_API = "http://localhost:3001/api";
 // DEPLOY
-export const BASE_URL = "http://localhost:3001";
-export const BASE_API = "http://localhost:3001/api";
+export const BASE_URL = "https://api-vietproshop.onrender.com";
+export const BASE_API = "https://api-vietproshop.onrender.com//api";
 export const URL = "https://vietproshop.onrender.com/";
