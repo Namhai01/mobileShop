acf0320a03ee06c240d0c7f34d5c52f2c121d23e
Namhai01 <73954521+Namhai01@users.noreply.github.com>
2024-05-28 20:47:50 +0700

    change link

--- src/shared/constants/app.jsx
+++ src/shared/constants/app.jsx
@@ -5,7 +5,7 @@
 // export const BASE_URL = "https://api-vietproshop.onrender.com";
 // export const BASE_API = "https://api-vietproshop.onrender.com/api";
 // export const URL = "https://vietproshop.onrender.com/";
-export const BASE_URL = "http://localhost:3001";
-export const BASE_API = "http://localhost:3001/api";
+export const BASE_URL = "https://vietproshop.onrender.com";
+export const BASE_API = "https://api-vietproshop.onrender.com/api";
 export const URL = "https://vietproshop.onrender.com/";
 // export const URL = "http://localhost:5173/";
