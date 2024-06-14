c12c43d5f10154fedab43a031b5517f6c84c7f13
Namhai01 <73954521+Namhai01@users.noreply.github.com>
2024-06-14 18:17:55 +0700

    update

--- src/shared/constants/app.jsx
+++ src/shared/constants/app.jsx
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://api-vietproshop.onrender.com";
 export const BASE_API = "https://api-vietproshop.onrender.com/api";
-export const URL = "https://vietproshop.onrender.com/";
+export const URL = "https://mobileshop-6ee3.onrender.com/";
